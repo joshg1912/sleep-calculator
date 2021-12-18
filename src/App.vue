ef49a72db4cc92c3e2d5ff830b05316ee79169ae
Josh G <joshgarcia7410@gmail.com>
2021-12-18 14:35:14 -0800

    adds getelelemntbyid and math functionality

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,11 @@
      <p>Monday <input id="day1" type="text"></p>
      <p>Tuesday <input id="day2" type="text"></p>
      <p>Wednesday <input id="day3" type="text"></p>
-      
+     <p>Thursday <input id="day4" type="text"></p>
+     <p>Friday <input id="day5" type="text"></p>
+     <p>Saturday <input id="day6" type="text"></p>
+     <p>Sunday <input id="day7" type="text"></p>
+     <p id="totalsleep"></p>
       </form>
     <button v-on:click="myFunction()">Calculate!</button>
   </div>
@@ -22,8 +26,12 @@ export default {
       let day1 = document.getElementById('day1').value;
       let day2 = document.getElementById('day2').value;
       let day3 = document.getElementById('day3').value;
-      const sum = parseFloat(day1) + parseFloat(day2) + parseFloat(day3);
-      window.alert(sum);
+      let day4 = document.getElementById('day4').value;
+      let day5 = document.getElementById('day5').value;
+      let day6 = document.getElementById('day6').value;
+      let day7 = document.getElementById('day7').value;
+      const sum = parseFloat(day1) + parseFloat(day2) + parseFloat(day3) + parseFloat(day4) + parseFloat(day5) + parseFloat(day6) + parseFloat(day7);
+      document.getElementById("totalsleep").innerHTML = `You sleep ${sum} hours a week`;
     }
 
   }
